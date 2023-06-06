e1554615476f3bd8330016569acb247d9fae4fd5
Emanuel José <75799134+emanueljsweb@users.noreply.github.com>
2023-06-06 08:44:38 -0300

    Alterações no Javascript
    Adaptação da lógica de geração de tabelas HTML e geração de números da cartela para atender as regras do jogo.

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
+// Inicializa uma variável que armazenará os dados das cartelas e jogadores.
 var cartelas = [];
 
-function geraNumerosAleatorios (tamanho, valorMaximo) {
+function geraNumerosAleatorios (tamanho, valorMinimo, valorMaximo) {
     var numeros = [];
 
     // Enquanto o Array não atingir o tamanho especificado, faça.
     while (numeros.length < tamanho) {
         // Gera um número aleatório.
-        var numeroAleatorio = Math.floor(Math.random() * valorMaximo);
+        var numeroAleatorio = Math.floor(Math.random() * (valorMaximo - valorMinimo) + valorMinimo);
         // Verifica se esse número não existe no Array, caso não exista, será inserido.
         if (!numeros.includes(numeroAleatorio)){
             numeros.push(numeroAleatorio);
@@ -21,7 +22,12 @@ function geraCartela () {
     var nomeJogador = prompt('Digite o nome do jogador.');
 
     // Gera números aleatórios.
-    var numeros = geraNumerosAleatorios(24,100);
+    var numeros = []
+    numeros.push(geraNumerosAleatorios(5, 0, 15));
+    numeros.push(geraNumerosAleatorios(5, 16, 30));
+    numeros.push(geraNumerosAleatorios(5, 31, 45));
+    numeros.push(geraNumerosAleatorios(5, 46, 60));
+    numeros.push(geraNumerosAleatorios(5, 61, 75));
 
     // Insere a cartea na página HTML.
     geraCartelaHTML(nomeJogador, numeros);
@@ -47,7 +53,7 @@ function geraCartelaHTML (nomeJogador, numeros) {
     h3.innerText = nomeJogador;
     div.appendChild(h3);
 
-    // Cria o elemento básico de tabela.
+    // Cria o elemento básico de tabela e seu cabeçalho.
     var table = document.createElement('table');
     table.innerHTML = '<thead><tr><td>B</td><td>I</td><td>N</td><td>G</td><td>O</td></tr></thead>';
     div.appendChild(table);
@@ -55,8 +61,6 @@ function geraCartelaHTML (nomeJogador, numeros) {
     // Cria o body da tabela
     var tbody = document.createElement('tbody');
     table.appendChild(tbody);
-
-    var index = 0;
 
     // Cria as linhas da tabela
     for (let linha = 0; linha < 5; linha++) {
@@ -66,12 +70,11 @@ function geraCartelaHTML (nomeJogador, numeros) {
             if (linha == 2 && coluna == 2) {
                 td.innerText = 'X';
             } else {
-                td.innerText = numeros[index];
-                index++;
+                td.innerText = numeros[coluna][linha];
             }
             tr.appendChild(td);
         }
-        tbody.appendChild(tr);        
+        tbody.appendChild(tr);
     }
 
     // Adiciona a div criada à div que armazena todas as cartelas.
